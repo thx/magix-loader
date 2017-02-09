0c4c86bf1a4e035f63800323ba3a61f92eac0338
xinglie.lkf <xinglie.lkf@taobao.com>
2017-02-09 20:16:32 +0800

    release 1.0.1

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 var combine = require('magix-combine');
+var query = require('querystring');
+var NumReg = /^\d+$/;
+
 combine.config({
-    loaderType: 'webpack'
+    loaderType: 'webpack',
+    disableMagixUpdater: true
 });
 module.exports = function(content) {
+    var options = this.query;
+    if (options) {
+        try {
+            options = JSON.parse(options.slice(1));
+        } catch (e) {
+            options = query.parse(options.slice(1));
+        }
+        for (var p in options) {
+            var val = options[p];
+            if (val === 'true') {
+                options[p] = true;
+            } else if (val === 'false') {
+                options[p] = false;
+            } else if (NumReg.test(val)) {
+                options[p] = parseInt(val, 10);
+            }
+        }
+        combine.config(options);
+    }
     this.cacheable();
     var cb = this.async();
     combine.processContent(this.resourcePath, '', content).then(function(c) {
