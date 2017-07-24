219bd98f4044dee82f68d5745cef7a9ebfedbe2a
weikang.zhuwk <weikang.zhuwk@alibaba-inc.com>
2017-07-24 20:14:13 +0800

    增加了对mx-view的处理

--- index.js
+++ index.js
@@ -1,19 +1,66 @@
 let combine = require('magix-combine');
 let loaderUtils = require('loader-utils');
+let fs = require('fs');
+let magixViewReg = /(?:mx|data)-view\s*=\s*\\?("[^"]*"|'[^']*'|[^'">\s]*)/g
+
 combine.config({
     loaderType: 'webpack',
     log: false
 });
-module.exports = function(content) {
+
+// 分析页面中有哪些view
+var collectViews = function (pageHtml) {
+    var results = [],
+        match, basename
+    while (match = magixViewReg.exec(pageHtml)) {
+        basename = match[1].replace(/'|"|\\/g, '')
+        results.push(basename)
+    }
+    return results
+}
+
+function analyzeViews(htmls) {
+    htmls = Array.isArray(htmls) ? htmls : [htmls]
+    let analyzeText = ''
+    htmls.forEach(function (html) {
+        analyzeText += fs.readFileSync(html)
+    })
+    return collectViews(analyzeText)
+}
+
+
+module.exports = function (content) {
     let context = this;
     let options = loaderUtils.getOptions(context) || {};
+    let addViewRequire = options.addViewRequire
+    let addViewAnalyze = options.addViewAnalyze
     combine.config(options);
     context.cacheable();
     let cb = context.async();
     combine.processContent(context.resourcePath, '', content, true).then(e => {
+        // console.log('e.tmplMxViews')
+        // console.log(e.tmplMxViewsArray)
         for (let p in e.fileDeps) {
             context.addDependency(p);
         }
+        let requireArrays = e.tmplMxViewsArray || []
+        let newRequireArray = []
+        // 针对资源文件，可以追加需要分析的文件
+        if (addViewAnalyze) {
+            let etHtmls = addViewAnalyze(context.resourcePath, e) || []
+            newRequireArray = analyzeViews(etHtmls)
+        }
+
+        requireArrays = requireArrays.concat(newRequireArray)
+        // 针对内容可以额外追加依赖
+        if (addViewRequire) {
+            requireArrays = requireArrays.concat(addViewRequire(e.content, e) || [])
+        }
+
+        requireArrays.forEach(function (element) {
+            e.content = 'require("' + element + '");\n' + e.content
+        })
+
         cb(null, e.content);
     }, err => { //增加的代码
         cb(err);
