e92e4da278f79585617e10af4f55621728b6d891
weikang.zhuwk <weikang.zhuwk@alibaba-inc.com>
2018-03-14 16:27:07 +0800

    feat(增加对addView的支持)

--- index.js
+++ index.js
@@ -65,15 +65,42 @@ module.exports = function(content) {
         if (addViewRequire) {
             requireArrays = requireArrays.concat(addViewRequire(e.content, e) || [])
         }
+        
+        let requirePromiseArray = []
 
         requireArrays.forEach(function(element) {
-          // 不需要重复添加
-          if(!new RegExp(element).test(e.content)){
-            e.content = 'require("' + element + '");\n' + e.content
-          }
+          
+          let resultPromise = new Promise(function(resolve, reject) {
+            // 针对cell组件的写法做一个特殊处理
+            let pathName = element
+            if (/@ali\/cell/.test(pathName)){
+              pathName = pathName.replace(/\@\d+\.\d+\.\d+/, '')
+            }
+            context.resolve(context.context, pathName, function(error,result){
+              // 无法找到目标路径的不要添加，不然直接报错了
+              if(!result){
+                resolve()
+                return
+              }
+              // 不需要重复添加
+              // 先看有没有写 require('xxxxx')
+              let currentRequireReg = new RegExp('require\\([\"|\']' + element + '[\"|\']\\)')
+              if(!currentRequireReg.test(e.content)){
+                e.content = 'require("' + element + '");\n' + e.content
+              }
+              // 再看有没有加addView，没加就补上
+              if(!new RegExp('Magix.addView\\([\"|\']'+element+'[\"|\']').test(e.content)){
+                e.content = e.content.replace(currentRequireReg,'Magix.addView("'+element+'",require("' + element + '"));')
+              }
+              resolve()
+            })
+          })
+          requirePromiseArray.push(resultPromise)
         })
 
-        cb(null, e.content);
+        Promise.all(requirePromiseArray).then(function() {
+          cb(null, e.content)
+        })
     }, err => { //增加的代码
         let deps = depsCache[context.resourcePath];
         if (deps) {
